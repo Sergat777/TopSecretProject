a6ba8ffa3d1b7a4bcbd3910ad1960efcbc91e443
Sergat 777 <sergey.kond777@gmail.com>
2022-11-23 19:57:03 +0300

    create base

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace GreatApparatusYebat
 {
@@ -23,6 +24,37 @@ namespace GreatApparatusYebat
         public MainWindow()
         {
             InitializeComponent();
+
+            movingTimer.Tick += MoveProjectile;
+            movingTimer.Start();
+
+            creatingTimer.Tick += CreateProjectile;
+            creatingTimer.Start();
+        }
+
+        DispatcherTimer movingTimer = new DispatcherTimer() { Interval = TimeSpan.FromMilliseconds(30) };
+        DispatcherTimer creatingTimer = new DispatcherTimer() { Interval = TimeSpan.FromMilliseconds(777) };
+
+        private void CreateProjectile(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MoveProjectile(object sender, EventArgs e)
+        {
+            Canvas.SetLeft(Enemy, Canvas.GetLeft(Enemy) + 3);
+        }
+
+        private void cnvsFightArea_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point mousePosition = Mouse.GetPosition(cnvsFightArea);
+            Canvas.SetLeft(rectangleHero, mousePosition.X);
+            Canvas.SetTop(rectangleHero, mousePosition.Y);
+        }
+
+        private void brdFight_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Close();
         }
     }
 }
